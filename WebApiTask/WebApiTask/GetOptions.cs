3ab8e645959c2b1ef3cd89ca20c65a889c2e3598
luhouzi12 <73565976+luhouzi12@users.noreply.github.com>
2020-11-03 17:43:09 +0800

    Submit WebApiTask

---
+++ WebApiTask/WebApiTask/GetOptions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApiTask
+{
+    public sealed class GetOptions
+    {
+
+        public bool CaseSensitive { get; set; }
+
+        [Required]
+        public string FilterCriteria { get; set; }
+
+    }
+}
---
+++ WebApiTask/WebApiTask/MemoryRepository.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiTask
+{
+    public class MemoryRepository:IRepository
+    {
+        private readonly Dictionary<long, ToDoItem> _dic = new Dictionary<long, ToDoItem>();
+
+        public MemoryRepository()
+        {
+            Init();
+        }
+
+        public async Task<ToDoItem> GetAsync(long id)
+        {
+            _dic.TryGetValue(id, out ToDoItem item);
+            return item;
+        }
+
+        public async Task UpsertAsync(ToDoItem model)
+        {
+            _dic[model.Id] = model;
+        }
+
+
+        public async Task<List<ToDoItem>> QueryAsync(
+            long id)
+        {
+            IEnumerable<ToDoItem> models = _dic.Values.ToList();
+            models = models.Where(v => v.Id == id);
+            return models.ToList();
+        }
+
+        public async Task<List<ToDoItem>> QueryAllAsync(
+        )
+        {
+            IEnumerable<ToDoItem> models = _dic.Values.ToList();
+            return models.ToList();
+        }
+
+       
+        private void Init()
+        {
+            for (var i = 0; i < 2; i++)
+            {
+                var item = new ToDoItem()
+                {
+                    Id = i,
+                    Name = $"test item {i}",
+                    IsComplete = false,
+                };
+                _dic[item.Id] = item;
+            }
+        }
+
+
+    }
+}
